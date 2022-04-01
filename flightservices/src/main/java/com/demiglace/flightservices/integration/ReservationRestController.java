94a03d3861bce94ddb271735ca01bff75408e0f3
demiglace0505 <demiglace0505@gmail.com>
2022-04-02 02:20:22 +0800

    finished java backend

--- flightservices/src/main/java/com/demiglace/flightservices/integration/ReservationRestController.java
+++ flightservices/src/main/java/com/demiglace/flightservices/integration/ReservationRestController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,6 +24,7 @@ import com.demiglace.flightservices.repos.PassengerRepository;
 import com.demiglace.flightservices.repos.ReservationRepository;
 
 @RestController
+@CrossOrigin
 public class ReservationRestController {
 
 	@Autowired
